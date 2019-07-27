d0345c101cc85e8014ac4904238e340bd9f01837
Nick B <nick@declension.net>
2019-07-27 12:17:41 +0100

    Strip skeleton of unncessary bits for scripting...

--- app/Main.hs
+++ app/Main.hs
@@ -1,11 +1,23 @@
+#!/usr/bin/env stack
+{- stack --resolver lts-13.9 script
+       --package text
+       --package lens
+       --package lens-regex-pcre
+-}
+{-# LANGUAGE QuasiQuotes #-}
+
 module Main where
 
-import           Control.Monad      (guard, liftM)
-import qualified Data.Text          as T
-import qualified Data.Text.IO       as TIO
-import           Lib
-import           System.Environment (getArgs)
-import Control.Arrow ((>>>))
+import           Control.Arrow          ((>>>))
+import           Control.Lens.Operators ((%~), (&), (^..))
+import           Control.Lens.Regex     (match, regex, rx)
+import           Control.Monad          (guard, liftM)
+import qualified Data.Text              as T
+import qualified Data.Text.IO           as TIO
+import           System.Environment     (getArgs)
+
+process :: T.Text -> T.Text
+process txt = txt & regex [rx|\b\w+|] . match %~ T.intersperse '-' . T.toUpper
 
 main :: IO ()
 main = do
